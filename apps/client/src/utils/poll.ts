42c7e8fd608bf94fce3298bc411f6a58dfe1c31e
Frank Parejo <francisco.parejo.lopez@gmail.com>
2020-07-12 19:49:11 +0200

    execute callback just after starting to poll data.

--- apps/client/src/utils/poll.ts
+++ apps/client/src/utils/poll.ts
@@ -3,6 +3,7 @@ let intervalID = 0;
 function createPoll(fn: () => void, interval = 1000): { start: () => void; restart: () => void; cancel: () => void } {
   return {
     start() {
+      fn();
       intervalID = setInterval(fn, interval);
     },
     restart() {
